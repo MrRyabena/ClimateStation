edd1ab8f7b5244de4c03e06b6979a8d8bad58518
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-04-20 12:43:10 +0300

    Added pins of the SDcard.

--- src/ClimateStation/settings.h
+++ src/ClimateStation/settings.h
@@ -1,7 +1,12 @@
 #pragma once
 
 
-#define MHZ19_TX 17
-#define MHZ19_RX 16
+static constexpr auto MHZ19_TX = 17;
+static constexpr auto MHZ19_RX = 16;
 
-#define BME280_CACHE_EXPIRATION_TIME 5000
+static constexpr auto BME280_CACHE_EXPIRATION_TIME = 5000;
+
+static constexpr auto SD_CS = 3;
+static constexpr auto SD_MOSI = 21;
+static constexpr auto SD_MISO = 16;
+static constexpr auto SD_SCK = 1;
