258d90ad9bdee8ba940aa649bdc145d9697d4f0a
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-04-21 00:35:29 +0300

    Testing MainWindow.

--- src/ClimateStation/ClimateStation.ino
+++ src/ClimateStation/ClimateStation.ino
@@ -6,8 +6,9 @@
 
 #include "shs_ClimateStation.h"
 #include "shs_ClimateStationVisualizer.h"
-#include "ClimateStationWidgets.h"
+#include "ClimateStationGUIcore.h"
 #include "shs_SDCard.h"
+#include "MainWindow.h"
 
 #include <SPI.h>
 #include <TFT_eSPI.h>       // https://github.com/Bodmer/TFT_eSPI
@@ -39,6 +40,8 @@ shs::SDCard sd(SD_CS, SD_MOSI, SD_MISO, SD_SCK);
 
 shs::RectWidget rw(tft_ptr, 20, 20, 100, 100, 20, 2, shs::ThemeColors::MINT);
 
+shs::MainWindow window(tft_ptr);
+
 void setup()
 {
   dinit();
@@ -49,22 +52,18 @@ void setup()
   climate_station_visualizer.start();
 
 
-     auto text = std::make_shared<shs::Label>(
-      tft_ptr,
-      30, 30, 
-      "HUMIDITY", 2, 1, 
-      shs::ThemeColors::GREEN
-    );
+  //    auto text = std::make_shared<shs::Label>(tft_ptr,
+  //     30, 30, 
+  //     "HUMIDITY", 7, 1, 
+  //     shs::ThemeColors::MINT, shs::ThemeColors::MINT, shs::ThemeColors::BACKGROUND
+  //   );
 
-    text->start();
+  //   text->start();
 
-    rw.attachLayer(std::make_shared<shs::RectWidget>(tft_ptr, text->x - 2, text->y - 2, text->width + 4, text->height + 4, 5, 4, shs::ThemeColors::MINT, shs::ThemeColors::MINT));
-    rw.attachLayer(text);
-
-
-
-
-
+  // //  rw.attachLayer(std::make_shared<shs::RectWidget>(tft_ptr, text->x - 2, text->y - 2, text->width + 4, text->height + 4, 5, 4, shs::ThemeColors::MINT, shs::ThemeColors::MINT));
+  //   rw.attachLayer(text, 0, 0, shs::Widget::Align::HORIZONTAL_CENTER | shs::Widget::Align::TOP, 0, -text->height / 2);
+  window.start();
+  window.tick();
 }
 
 
@@ -78,25 +77,27 @@ void loop()
      { 
       
 
-        climate_station_visualizer.tick();
-        Wire.end();
-        sd.begin();
+      //   climate_station_visualizer.tick();
+      //   Wire.end();
+      //   sd.begin();
         
-       climate_station_visualizer.printDebug(String("SD status: ") + static_cast<int>(shs::etoi(sd.getStatus())), 20, 20);
+      //  climate_station_visualizer.printDebug(String("SD status: ") + static_cast<int>(shs::etoi(sd.getStatus())), 20, 20);
 
-       if (sd.getStatus() == shs::SDCard::Status::CARD_OK)
-        {
-          climate_station_visualizer.printDebug(sd.readTest(), 20, 35);
+      //  if (sd.getStatus() == shs::SDCard::Status::CARD_OK)
+      //   {
+      //     climate_station_visualizer.printDebug(sd.readTest(), 20, 35);
 
-            //climate_station_visualizer.printDebug(String("Card type: ") + SD.cardType(), 20, 48);
-            //climate_station_visualizer.printDebug(String("Card size: ") + SD.cardSize(), 20, 76);
-           // climate_station_visualizer.printDebug(String("Total bytes: ") + SD.totalBytes(), 20, 104);
-           // climate_station_visualizer.printDebug(String("Used bytes: ") + SD.usedBytes(), 20, 132);
-       }
+      //       //climate_station_visualizer.printDebug(String("Card type: ") + SD.cardType(), 20, 48);
+      //       //climate_station_visualizer.printDebug(String("Card size: ") + SD.cardSize(), 20, 76);
+      //      // climate_station_visualizer.printDebug(String("Total bytes: ") + SD.totalBytes(), 20, 104);
+      //      // climate_station_visualizer.printDebug(String("Used bytes: ") + SD.usedBytes(), 20, 132);
+      //  }
 
-        sd.end();
-        Wire.begin();
-        rw.tick();
+      //   sd.end();
+      //   Wire.begin();
+      //   rw.tick();
+
+      window.tick();
         tmr.reset();
      }
 }
