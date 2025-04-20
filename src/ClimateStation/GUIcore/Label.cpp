b99aecf1a82e5a66c94db4ccedde3c15f36db4a8
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-04-21 00:34:20 +0300

    Added frame for the text.

--- src/ClimateStation/GUIcore/Label.cpp
+++ src/ClimateStation/GUIcore/Label.cpp
@@ -2,14 +2,19 @@
 
 
 shs::Label::Label(
-    const std::shared_ptr<TFT_eSPI> tft, const shs::t::shs_coord_t x, const shs::t::shs_coord_t y,
+    const std::shared_ptr<TFT_eSPI> tft,
+    const shs::t::shs_coord_t x, const shs::t::shs_coord_t y,
     const shs::t::shs_string_t& str,
     const shs::t::shs_font_t set_font, const shs::t::shs_coord_t set_text_size,
-    const shs::t::shs_color_t set_background_color, const shs::t::shs_color_t set_foreground_color
+    const shs::t::shs_color_t set_background_color, const shs::t::shs_color_t set_frame_color,
+    const shs::t::shs_color_t set_foreground_color
 )
     : Widget(tft, x, y, 0, 0), m_text(str), font(set_font), text_size(set_text_size),
-    background_color(set_background_color), foreground_color(set_foreground_color)
+    background_color(set_background_color), foreground_color(set_foreground_color),
+    frame_margin(4), frame_stroke(1), frame_radius(5), frame_color(set_frame_color)
 {}
+
+
 
 
 void shs::Label::start()
@@ -27,10 +32,20 @@ void shs::Label::tick()
 {
     if (!m_tft) return;
 
+    shs::RectWidget frame(m_tft, x - frame_margin, y - frame_margin,
+        width + frame_margin * 2, height + frame_margin * 2,
+        frame_radius, frame_stroke,
+        frame_color, background_color
+    );
+
+    frame.tick();
+
     m_tft->setTextColor(shs::utils::rgb565(foreground_color), shs::utils::rgb565(background_color));
+
     m_tft->setTextFont(font);
     m_tft->setTextSize(text_size);
     m_tft->setCursor(x, y);
+
     m_tft->print(m_text);
 
     Widget::tick();
--- src/ClimateStation/GUIcore/Label.h
+++ src/ClimateStation/GUIcore/Label.h
@@ -2,6 +2,7 @@
 
 
 #include "Widget.h"
+#include "RectWidget.h"
 
 #include <shs_types.h>
 #include "types.h"
@@ -18,6 +19,10 @@ class shs::Label : public shs::Widget
 public:
     shs::t::shs_font_t font;
     shs::t::shs_coord_t text_size;
+    shs::t::shs_coord_t frame_margin;
+    shs::t::shs_coord_t frame_stroke;
+    shs::t::shs_coord_t frame_radius;
+    shs::t::shs_color_t frame_color;
     shs::t::shs_color_t background_color;
     shs::t::shs_color_t foreground_color;
 
@@ -26,6 +31,7 @@ public:
         const shs::t::shs_string_t& str,
         const shs::t::shs_font_t set_font, const shs::t::shs_coord_t set_text_size,
         const shs::t::shs_color_t set_background_color = shs::ThemeColors::BACKGROUND,
+        const shs::t::shs_color_t set_frame_color = shs::ThemeColors::MINT,
         const shs::t::shs_color_t set_foreground_color = shs::ThemeColors::LIGHT
     );
 
