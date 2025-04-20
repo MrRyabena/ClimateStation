f0991d880ff4062b076af745bdc3e8cb421a9c98
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-04-20 12:55:46 +0300

    Create types.h

---
+++ src/ClimateStation/GUIcore/types.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdint.h>
+
+
+namespace shs
+{
+    namespace t
+    {
+        using shs_coord_t = int16_t;
+        using shs_color_t = uint32_t;
+        using shs_font_t = uint16_t;
+    }
+}
