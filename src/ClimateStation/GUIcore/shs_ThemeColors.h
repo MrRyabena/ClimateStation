19dbf57958f273682403e4ab4b36e3f5e7113f4c
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-04-20 12:54:44 +0300

    A structure with SmartHomeSystem colors has been created.

---
+++ src/ClimateStation/GUIcore/shs_ThemeColors.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+namespace shs
+{
+    struct ThemeColors
+    {
+        static constexpr auto BACKGROUND = 0x001219;
+        static constexpr auto BACKGROUND_LIGHT = 0x005f73;
+        static constexpr auto MINT = 0x0a9396;
+        static constexpr auto LIGHT_MINT = 0x94d2bd;
+        static constexpr auto LIGHT = 0xe9d8a6;
+        static constexpr auto ORANGE = 0xee9b00;
+        static constexpr auto DARK_ORANGE = 0xc57d11;
+        static constexpr auto LIGHT_GREEN = 0xabbe40;
+        static constexpr auto GREEN = 0x78b910;
+        static constexpr auto DARK_GREEN = 487808;
+    };
+}
