89db9e3319ec4d5bdaa9407b04d9e09722ed20c1
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-04-20 12:57:30 +0300

    The graphics core inclusion file.

---
+++ src/ClimateStation/ClimateStationWidgets.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "GUIcore/Widget.h"
+#include "GUIcore/Widget.cpp"
+#include "GUIcore/RectWidget.h"
+#include "GUIcore/RectWidget.cpp"
+#include "GUIcore/utils.h"
+#include "GUIcore/shs_ThemeColors.h"
+#include "GUIcore/Label.h"
+#include "GUIcore/Label.cpp"
