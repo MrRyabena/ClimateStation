79a8c755b6869633b88f7c466c19cc572698b0f9
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-04-22 00:44:48 +0300

    Redesigned for more flexible customization.

--- src/ClimateStation/GUIcore/Label.cpp
+++ src/ClimateStation/GUIcore/Label.cpp
@@ -2,49 +2,68 @@
 
 
 shs::Label::Label(
-    const std::shared_ptr<TFT_eSPI> tft,
-    const shs::t::shs_coord_t x, const shs::t::shs_coord_t y,
+    std::shared_ptr<TFT_eSPI> tft,
     const shs::t::shs_string_t& str,
     const shs::t::shs_font_t set_font, const shs::t::shs_coord_t set_text_size,
+    const shs::t::shs_coord_t x, const shs::t::shs_coord_t y,
+    const shs::t::shs_coord_t set_width, const shs::t::shs_coord_t set_height,
+    const shs::t::shs_coord_t set_radius, const shs::t::shs_coord_t set_stroke,
     const shs::t::shs_color_t set_background_color, const shs::t::shs_color_t set_frame_color,
     const shs::t::shs_color_t set_foreground_color
 )
     : Widget(tft, x, y, 0, 0), m_text(str), font(set_font), text_size(set_text_size),
     background_color(set_background_color), foreground_color(set_foreground_color),
-    frame_margin(4), frame_stroke(1), frame_radius(5), frame_color(set_frame_color)
-{}
+    frame_stroke(set_stroke), frame_radius(set_radius), frame_color(set_frame_color),
+    text_align(shs::Widget::Align::HORIZONTAL_CENTER | shs::Widget::Align::VERTICAL_CENTER)
+{
+
+    if (!m_tft) return;
+
+    m_tft->setTextFont(font);
+    m_tft->setTextSize(text_size);
+
+    text_width = m_tft->textWidth(m_text);
+    text_height = m_tft->fontHeight();
+
+    width = set_width ? set_width : text_width;
+    height = set_height ? set_height : text_height;
+}
 
 
 
 
 void shs::Label::start()
-{
-    if (!m_tft) return;
-    m_tft->setTextFont(font);
-    m_tft->setTextSize(text_size);
-
-    width = m_tft->textWidth(m_text);
-    height = m_tft->fontHeight();
-}
+{}
 
 
 void shs::Label::tick()
 {
     if (!m_tft) return;
 
-    shs::RectWidget frame(m_tft, x - frame_margin, y - frame_margin,
-        width + frame_margin * 2, height + frame_margin * 2,
-        frame_radius, frame_stroke,
-        frame_color, background_color
-    );
-
-    frame.tick();
-
     m_tft->setTextColor(shs::utils::rgb565(foreground_color), shs::utils::rgb565(background_color));
 
     m_tft->setTextFont(font);
     m_tft->setTextSize(text_size);
-    m_tft->setCursor(x, y);
+
+    if (width == text_width && height == text_height)
+    {
+        m_tft->setCursor(x, y);
+    }
+    else
+    {
+        shs::RectWidget frame(m_tft, x, y,
+            width, height,
+            frame_radius, frame_stroke,
+            frame_color, background_color
+        );
+
+        frame.tick();
+
+        auto coords = shs::Widget::getAligned(frame, shs::Widget(m_tft, 0, 0, text_width, text_height), text_align);
+
+        m_tft->setCursor(coords.first, coords.second);
+
+    }
 
     m_tft->print(m_text);
 
--- src/ClimateStation/GUIcore/Label.h
+++ src/ClimateStation/GUIcore/Label.h
@@ -19,17 +19,22 @@ class shs::Label : public shs::Widget
 public:
     shs::t::shs_font_t font;
     shs::t::shs_coord_t text_size;
-    shs::t::shs_coord_t frame_margin;
+    shs::t::shs_coord_t text_width;
+    shs::t::shs_coord_t text_height;
     shs::t::shs_coord_t frame_stroke;
     shs::t::shs_coord_t frame_radius;
     shs::t::shs_color_t frame_color;
     shs::t::shs_color_t background_color;
     shs::t::shs_color_t foreground_color;
+    uint8_t text_align;
 
 
-    Label(const std::shared_ptr<TFT_eSPI> tft, const shs::t::shs_coord_t x, const shs::t::shs_coord_t y,
+    Label(std::shared_ptr<TFT_eSPI> tft,
         const shs::t::shs_string_t& str,
         const shs::t::shs_font_t set_font, const shs::t::shs_coord_t set_text_size,
+        const shs::t::shs_coord_t x = 0, const shs::t::shs_coord_t y = 0,
+        const shs::t::shs_coord_t set_width = 0, const shs::t::shs_coord_t set_height = 0,
+        const shs::t::shs_coord_t set_radius = 0, const shs::t::shs_coord_t set_stroke = 0,
         const shs::t::shs_color_t set_background_color = shs::ThemeColors::BACKGROUND,
         const shs::t::shs_color_t set_frame_color = shs::ThemeColors::MINT,
         const shs::t::shs_color_t set_foreground_color = shs::ThemeColors::LIGHT
