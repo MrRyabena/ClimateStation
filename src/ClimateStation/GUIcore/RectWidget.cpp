53ba95b76ff4bb05f75fe961654407853da12577
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-04-22 00:46:38 +0300

    The outline drawing algorithm has been redesigned, rendering has been made into a separate function.

--- src/ClimateStation/GUIcore/RectWidget.cpp
+++ src/ClimateStation/GUIcore/RectWidget.cpp
@@ -1,7 +1,7 @@
 #include "RectWidget.h"
 
 shs::RectWidget::RectWidget(
-    const std::shared_ptr<TFT_eSPI> tft, const shs::t::shs_coord_t x, const shs::t::shs_coord_t y,
+    std::shared_ptr<TFT_eSPI> tft, const shs::t::shs_coord_t x, const shs::t::shs_coord_t y,
     const shs::t::shs_coord_t width, const shs::t::shs_coord_t height,
     const shs::t::shs_coord_t set_radius, const shs::t::shs_coord_t set_stroke,
     const shs::t::shs_color_t set_stroke_color,
@@ -11,13 +11,20 @@ shs::RectWidget::RectWidget(
     stroke_color(set_stroke_color), background_color(set_background_color), foreground_color(set_foreground_color)
 {}
 
+
 void shs::RectWidget::tick()
+{
+    draw();
+    Widget::tick();
+}
+
+void shs::RectWidget::draw()
 {
     if (!m_tft) return;
 
-    m_tft->fillRoundRect(x, y, width, height, radius, shs::utils::rgb565(stroke_color));
-    m_tft->fillRoundRect(x + stroke, y + stroke, width - 2 * stroke, height - 2 * stroke, radius - stroke, shs::utils::rgb565(background_color));
+    for (auto i = 1; i <= stroke; ++i)
+        m_tft->drawRoundRect(x - i, y - i, width + 2 * i, height + 2 * i, radius + i, shs::utils::rgb565(stroke_color));
 
-    Widget::tick();
+    m_tft->fillRoundRect(x, y, width, height, radius, shs::utils::rgb565(background_color));
 }
 
--- src/ClimateStation/GUIcore/RectWidget.h
+++ src/ClimateStation/GUIcore/RectWidget.h
@@ -9,6 +9,9 @@
 #include "types.h"
 #include "utils.h"
 
+#define DEBUG
+#define SHS_SF_DEBUG
+#include <shs_debug.h>
 namespace shs
 {
     class RectWidget;
@@ -24,7 +27,7 @@ public:
     shs::t::shs_color_t background_color;
     shs::t::shs_color_t foreground_color;
 
-    RectWidget(const std::shared_ptr<TFT_eSPI> tft,
+    RectWidget(std::shared_ptr<TFT_eSPI> tft,
         const shs::t::shs_coord_t x, const shs::t::shs_coord_t y,
         const shs::t::shs_coord_t width, const shs::t::shs_coord_t height,
         const shs::t::shs_coord_t set_radius, const shs::t::shs_coord_t set_stroke,
@@ -35,6 +38,8 @@ public:
 
 
     void tick() override;
+
+    void draw();
 
 };
 
