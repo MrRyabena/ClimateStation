acdf7057a8494c0b98b7f09ff95e7d296a661dec
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-05-03 14:22:33 +0300

    Pins have been changed according to the new scheme.

--- src/ClimateStation/settings.h
+++ src/ClimateStation/settings.h
@@ -4,9 +4,11 @@
 static constexpr auto MHZ19_TX = 17;
 static constexpr auto MHZ19_RX = 35;
 
+static constexpr auto TFT_LED = 19;
+
 static constexpr auto BME280_CACHE_EXPIRATION_TIME = 5000;
 
 static constexpr auto SD_CS = 3;
-static constexpr auto SD_MOSI = 21;
-static constexpr auto SD_MISO = 16;
-static constexpr auto SD_SCK = 1;
+static constexpr auto SD_MOSI = 23;
+static constexpr auto SD_MISO = 34;
+static constexpr auto SD_SCK = 18;
