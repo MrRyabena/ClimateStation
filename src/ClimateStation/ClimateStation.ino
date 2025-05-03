333f1405b8c8d4e0e6c677fda74d7554d4144c5d
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-05-03 14:24:14 +0300

    Testing new changes.

--- src/ClimateStation/ClimateStation.ino
+++ src/ClimateStation/ClimateStation.ino
@@ -1,11 +1,16 @@
 #include "settings.h"
 
+#define DEBUG
+#define SHS_SF_DEBUG
+#include <shs_settings_private.h>
+
 #include <shs_debug.h>
 
 #include <memory>
 
 #include "shs_ClimateStation.h"
 #include "shs_ClimateStationVisualizer.h"
+#include "shs_ClimateStationStorage.h"
 #include "ClimateStationGUIcore.h"
 #include "shs_SDCard.h"
 #include "MainWindow.h"
@@ -21,7 +26,7 @@
 
 auto tft_ptr = std::make_shared<TFT_eSPI>();
 
-shs::ClimateStation climate_station(
+auto climate_station = std::make_shared<shs::ClimateStation>(
     std::make_shared<shs::MHZ19>(MHZ19_RX, MHZ19_TX),
     std::make_shared<shs::BME280>(BME280_CACHE_EXPIRATION_TIME),
     nullptr,
@@ -29,14 +34,16 @@ shs::ClimateStation climate_station(
     std::make_shared<GyverNTP>(shs::settings::GMT)
 );
 
+auto storage = std::make_shared<shs::ClimateStationStorage>(climate_station, SD_CS);
 
 shs::ClimateStationVisualizer climate_station_visualizer(
     climate_station,
-    tft_ptr
+    storage,
+    tft_ptr, TFT_LED
 );
 
 
-shs::SDCard sd(SD_CS, SD_MOSI, SD_MISO, SD_SCK);
+
 
 shs::RectWidget rw(tft_ptr, 20, 20, 100, 100, 20, 2, shs::ThemeColors::MINT);
 
@@ -45,32 +52,34 @@ shs::MainWindow window(tft_ptr);
 void setup()
 {
   dinit();
-  
+
   shs::ControlWiFi::connectWiFi();
 
-  climate_station.start();
+  
+  SPI.begin(SD_SCK, SD_MISO, SD_MOSI);
+  dout(TFT_MISO); dout(TFT_MOSI); doutln(TFT_SCLK);
+  storage->start();
+
+
+  climate_station->start();
   climate_station_visualizer.start();
 
-
-  //    auto text = std::make_shared<shs::Label>(tft_ptr,
-  //     30, 30, 
-  //     "HUMIDITY", 7, 1, 
-  //     shs::ThemeColors::MINT, shs::ThemeColors::MINT, shs::ThemeColors::BACKGROUND
-  //   );
-
-  //   text->start();
-
-  // //  rw.attachLayer(std::make_shared<shs::RectWidget>(tft_ptr, text->x - 2, text->y - 2, text->width + 4, text->height + 4, 5, 4, shs::ThemeColors::MINT, shs::ThemeColors::MINT));
-  //   rw.attachLayer(text, 0, 0, shs::Widget::Align::HORIZONTAL_CENTER | shs::Widget::Align::TOP, 0, -text->height / 2);
   window.start();
   window.tick();
+  window.updateData(climate_station->getData());
+
+
+  doutln("ok");
 }
 
 
 shs::ProgramTime tmr;
 void loop()
 {
-    climate_station.tick();
+    climate_station->tick();
+    storage->tick();
+    climate_station_visualizer.tick();
+
 
 
      if (tmr.milliseconds() >= 10'000)
@@ -82,22 +91,23 @@ void loop()
         
       //  climate_station_visualizer.printDebug(String("SD status: ") + static_cast<int>(shs::etoi(sd.getStatus())), 20, 20);
 
-      //  if (sd.getStatus() == shs::SDCard::Status::CARD_OK)
-      //   {
-      //     climate_station_visualizer.printDebug(sd.readTest(), 20, 35);
+      // if (storage.getStatus() == shs::ClimateStationStorage::Status::CARD_OK)
+      //  {
+      //     doutln(storage.readTest());
 
-      //       //climate_station_visualizer.printDebug(String("Card type: ") + SD.cardType(), 20, 48);
-      //       //climate_station_visualizer.printDebug(String("Card size: ") + SD.cardSize(), 20, 76);
-      //      // climate_station_visualizer.printDebug(String("Total bytes: ") + SD.totalBytes(), 20, 104);
-      //      // climate_station_visualizer.printDebug(String("Used bytes: ") + SD.usedBytes(), 20, 132);
-      //  }
+      //     doutln(String("Card type: ") + SD.cardType());
+      //     doutln(String("Card size: ") + SD.cardSize());
+      //    doutln(String("Total bytes: ") + SD.totalBytes());
+      // doutln(String("Used bytes: ") + SD.usedBytes());
+      //   }
 
       //   sd.end();
       //   Wire.begin();
       //   rw.tick();
 
-      window.tick();
-      window.updateData(climate_station.getData());
+      //window.tick();
+      window.updateData(climate_station->getData());
       tmr.reset();
+      // storage.end();
      }
 }
