ca0f8dc3f0cfe02e80a8451aade6c1f9a83884db
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-05-03 14:15:37 +0300

    Redesigned using std::shared_ptr. Added touch calibration. Added display sleep.

--- src/ClimateStation/shs_ClimateStationVisualizer.cpp
+++ src/ClimateStation/shs_ClimateStationVisualizer.cpp
@@ -1,39 +1,68 @@
 #include "shs_ClimateStationVisualizer.h"
 
-shs::ClimateStationVisualizer::ClimateStationVisualizer(shs::ClimateStation& climate_station, std::shared_ptr<TFT_eSPI> tft)
-    : m_cls(climate_station), m_tft(tft)
+
+shs::ClimateStationVisualizer::ClimateStationVisualizer(std::shared_ptr<shs::ClimateStation> climate_station,
+    std::shared_ptr<shs::ClimateStationStorage> storage, std::shared_ptr<TFT_eSPI> m_tft,
+    const uint8_t tft_led_pin
+)
+    : m_cls(climate_station), m_storage(storage), m_tft(m_tft), m_tft_LED_pin(tft_led_pin)
 {}
 
 void shs::ClimateStationVisualizer::start()
 {
-    if (m_tft)
-    {
-        m_tft->init();
-        m_tft->setRotation(3);
-        m_tft->fillScreen(m_rgb565(shs::ThemeColors::BACKGROUND));
-    }
+    enableTFT();
+
+    if (!m_tft) return;
+
+    m_tft->init();
+    m_tft->setRotation(3);
+    m_tft->fillScreen(m_rgb565(shs::ThemeColors::BACKGROUND));
+
+    m_touch_calibrate();
+
 }
 
 void shs::ClimateStationVisualizer::tick()
 {
+    uint16_t x = 0, y = 0; // To store the touch coordinates
+
+    // Pressed will be set true is there is a valid touch on the screen
+    bool pressed = m_tft->getTouch(&x, &y);
+
+    // Draw a white spot at the detected coordinates
+    if (pressed)
+    {
+        m_sleep_tmr.reset();
+        doutln("pressed");
+
+        //tft_ptr->fillCircle(x, y, 2, TFT_WHITE);
+        //Serial.print("x,y = ");
+        //Serial.print(x);
+        //Serial.print(",");
+        //Serial.println(y);
+    }
+
+    if (m_sleep_tmr.milliseconds() >= m_SLEEP_TIMEOUT) disableTFT();
+    else enableTFT();
+
     if (m_main_tmr.milliseconds() >= TICK_TIME)
     {
-        if (m_tft)
-        {
-            m_tft->fillScreen(m_rgb565(0x00, 0x12, 0x19));
+        // if (m_tft)
+        // {
+        //     m_tft->fillScreen(m_rgb565(0x00, 0x12, 0x19));
 
-            auto data = m_cls.getData();
+        //     auto data = m_cls->getData();
 
-            String str = String("CO2:                  ") + data.CO2; m_tft->drawString(str, 20, 48, 2);
-            str = String("pressure:             ") + static_cast<float>(data.pressure); m_tft->drawString(str, 20, 63, 2);
-            str = String("indoor_temperature:   ") + static_cast<float>(data.indoor_temperature); m_tft->drawString(str, 20, 76, 2);
-            str = String("outdoor_temperature:  ") + static_cast<float>(data.outdoor_temperature); m_tft->drawString(str, 20, 104, 2);
-            str = String("indoor_humidity:      ") + static_cast<float>(data.indoor_humidity); m_tft->drawString(str, 20, 132, 2);
-            str = String("outdoor_humidity:     ") + static_cast<float>(data.outdoor_humidity); m_tft->drawString(str, 20, 160, 2);
-            str = String("time:                 ") + data.time; m_tft->drawString(str, 20, 188, 2);
+        //     String str = String("CO2:                  ") + data.CO2; m_tft->drawString(str, 20, 48, 2);
+        //     str = String("pressure:             ") + static_cast<float>(data.pressure); m_tft->drawString(str, 20, 63, 2);
+        //     str = String("indoor_temperature:   ") + static_cast<float>(data.indoor_temperature); m_tft->drawString(str, 20, 76, 2);
+        //     str = String("outdoor_temperature:  ") + static_cast<float>(data.outdoor_temperature); m_tft->drawString(str, 20, 104, 2);
+        //     str = String("indoor_humidity:      ") + static_cast<float>(data.indoor_humidity); m_tft->drawString(str, 20, 132, 2);
+        //     str = String("outdoor_humidity:     ") + static_cast<float>(data.outdoor_humidity); m_tft->drawString(str, 20, 160, 2);
+        //     str = String("time:                 ") + data.time; m_tft->drawString(str, 20, 188, 2);
 
 
-        }
+        // }
 
         m_main_tmr.reset();
     }
@@ -44,5 +73,60 @@ void shs::ClimateStationVisualizer::printDebug(const String& str, const uint16_t
     if (m_tft) m_tft->drawString(str, x, y, 2);
 }
 
+void shs::ClimateStationVisualizer::enableTFT()
+{
+    if (!m_tft_enabled)
+    {
+        pinMode(m_tft_LED_pin, OUTPUT);
+        digitalWrite(m_tft_LED_pin, LOW);
 
+        m_tft_enabled = true;
+    }
+}
+
+void shs::ClimateStationVisualizer::disableTFT()
+{
+
+    if (m_tft_enabled)
+    {
+        pinMode(m_tft_LED_pin, INPUT);
+        m_tft_enabled = false;
+    }
+}
+
+
+void shs::ClimateStationVisualizer::m_touch_calibrate()
+{
+    uint16_t calData[shs::ClimateStationStorage::m_TFT_TOUCH_CALIBRATION_DATA_SIZE];
+
+    if (m_storage->get_TFT_calData(calData))
+    {
+        m_tft->setTouch(calData);
+        return;
+    }
+
+
+    // Calibrate
+    m_tft->fillScreen(TFT_BLACK);
+    m_tft->setCursor(20, 0);
+    m_tft->setTextFont(2);
+    m_tft->setTextSize(1);
+    m_tft->setTextColor(TFT_WHITE, TFT_BLACK);
+
+    m_tft->println("Touch corners as indicated");
+
+    m_tft->setTextFont(1);
+    m_tft->println();
+
+    m_tft->calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
+
+    if (m_storage) m_storage->save_TFT_calData(calData);
+
+    m_tft->fillScreen(TFT_BLACK);
+
+    m_tft->setTextColor(TFT_GREEN, TFT_BLACK);
+    m_tft->println("Calibration complete!");
+
+    delay(4000);
+}
 
--- src/ClimateStation/shs_ClimateStationVisualizer.h
+++ src/ClimateStation/shs_ClimateStationVisualizer.h
@@ -2,7 +2,7 @@
 
 
 #include <memory>
-
+#include <Arduino.h>
 //#include <microLED.h>
 
 #include <SPI.h>
@@ -12,6 +12,8 @@
 #include <shs_Process.h>
 #include <shs_ProgramTime.h>
 
+#include "shs_ClimateStation.h"
+#include "shs_ClimateStationStorage.h"
 #include "GUIcore/shs_ThemeColors.h"
 
 
@@ -21,8 +23,11 @@ public:
     static constexpr auto TICK_TIME = 10'000;
 
     ClimateStationVisualizer(
-        shs::ClimateStation& climate_station,
-        std::shared_ptr<TFT_eSPI> tft//,
+        std::shared_ptr<shs::ClimateStation> climate_station,
+        std::shared_ptr<shs::ClimateStationStorage> storage,
+        std::shared_ptr<TFT_eSPI> tft,
+        uint8_t tft_led_pin
+        //,
         //    std::shared_ptr<microLED> aled
     );
 
@@ -38,18 +43,32 @@ public:
 
     void printDebug(const String& str, const uint16_t x, const uint16_t y);
 
+    void enableTFT();
+    void disableTFT();
+
 protected:
+
     std::shared_ptr<TFT_eSPI> m_tft;
-    shs::ClimateStation& m_cls;
+    std::shared_ptr<shs::ClimateStation> m_cls;
+    std::shared_ptr<shs::ClimateStationStorage> m_storage;
+
     //    std::shared_ptr<microLED> m_aled;
 
     shs::ProgramTime m_main_tmr;
+    shs::ProgramTime m_sleep_tmr;
 
-    constexpr uint16_t m_rgb565(const uint8_t r, const uint8_t g, const uint8_t b) const 
+    static constexpr auto m_SLEEP_TIMEOUT = 10000;     // 10 seconds
+
+    const uint8_t m_tft_LED_pin;
+    bool m_tft_enabled{};
+
+
+    constexpr uint16_t m_rgb565(const uint8_t r, const uint8_t g, const uint8_t b) const
     {
         return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
     }
 
     constexpr uint16_t m_rgb565(const uint32_t rgb) { return m_rgb565(rgb >> 16, rgb >> 8, rgb); }
 
+    void m_touch_calibrate();
 };
