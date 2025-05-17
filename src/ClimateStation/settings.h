196b9d069280c993e501ed4d8339444996aa84d8
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-05-18 01:55:14 +0300

    Values have been moved to the config.

--- src/ClimateStation/settings.h
+++ src/ClimateStation/settings.h
@@ -1,19 +1,10 @@
 #pragma once
 
-
-static constexpr auto MHZ19_TX = 17;
-static constexpr auto MHZ19_RX = 35;
-
-static constexpr auto DHT_PIN = 1;
-
-static constexpr auto TFT_LED = 19;
-
 static constexpr auto LEDS_NUM = 9 * 5;
 static constexpr auto LEDS_PIN = 16;
-
-static constexpr auto BME280_CACHE_EXPIRATION_TIME = 5000;
 
 static constexpr auto SD_CS = 3;
 static constexpr auto SD_MOSI = 23;
 static constexpr auto SD_MISO = 34;
 static constexpr auto SD_SCK = 18;
+
