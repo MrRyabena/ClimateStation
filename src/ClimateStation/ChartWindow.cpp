0cdbfdf7e3e69c7d909fa6d0f0629d3d54ebc518
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-05-19 01:00:16 +0300

    Added CO2_chart.

--- src/ClimateStation/ChartWindow.cpp
+++ src/ClimateStation/ChartWindow.cpp
@@ -1,8 +1,10 @@
 #include "ChartWindow.h"
 
-shs::ChartWindow::ChartWindow(std::shared_ptr<TFT_eSPI> tft, std::shared_ptr<shs::ClimateStationStorage> storage)
-    : Widget(tft, 0, 0, 320, 240), m_storage(storage)
+shs::ChartWindow::ChartWindow(std::shared_ptr<TFT_eSPI> tft, std::shared_ptr<shs::ClimateStation> climate_station, std::shared_ptr<shs::ClimateStationStorage> storage)
+    : Widget(tft, 0, 0, 320, 240), m_climate_station(climate_station), m_storage(storage)
 {}
+
+
 
 
 void shs::ChartWindow::start()
@@ -13,7 +15,19 @@ void shs::ChartWindow::start()
     auto block = std::make_shared<shs::RectWidget>(m_tft, 0, 0, m_BUTTON_SIZE, m_BUTTON_SIZE, 8, 2);
     attachLayer(block, shs::Widget::Align::LEFT | shs::Widget::Align::TOP, 10, 10);
     block->attachLayer(std::make_shared<shs::Image>(m_storage, F("home_icon_30.shsf"), m_tft), shs::Widget::Align::CENTER);
+    
+    
+
+    auto analyzer = std::make_shared<shs::ChartAnalyzer>(m_storage, width - 20, height - 40);
+
+    auto fname = shs::ClimateStationStorage::m_getDateFileName(m_climate_station->getData().time);
+    analyzer->addFile(fname);
+    
+    attachLayer(std::make_shared<shs::CO2_chart>(m_storage, m_tft, analyzer, 0, 0, width - 10, height - 60), shs::Widget::Align::BOTTOM | shs::Widget::LEFT, 5, 5);
+    
     buttons.push_back(block);
+
+    
 
     Widget::start();
     Widget::tick();
