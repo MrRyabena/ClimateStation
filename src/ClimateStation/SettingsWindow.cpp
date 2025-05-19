32c4af8c8fd61df1fc51839336163ddb50de96e1
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-05-19 20:16:40 +0300

    Added interface.

--- src/ClimateStation/SettingsWindow.cpp
+++ src/ClimateStation/SettingsWindow.cpp
@@ -13,8 +13,88 @@ void shs::SettingsWindow::start()
     attachLayer(block, shs::Widget::Align::LEFT | shs::Widget::Align::TOP, 10, 10);
     block->attachLayer(std::make_shared<shs::Image>(m_storage, F("home_icon_30.shsf"), m_tft), shs::Widget::Align::CENTER);
 
+    buttons.push_back(block);
 
+
+    // LED brightness
+    m_vc.push_back(std::make_shared<shs::ValueControl<uint16_t>>(
+        m_storage->cs_config.LED_BRIGHTNESS, 10,
+        m_tft,
+        0, 0, 150, 40
+    ));
+
+    attachLayer(m_vc.back(), shs::Widget::Align::RIGHT | shs::Widget::Align::TOP, 10, 60);
+
+    attachLayer(std::make_shared<shs::Label>(
+        m_tft, F("LED brightness (0-255):"),
+        2, 1
+    ),
+        shs::Widget::LEFT | shs::Widget::TOP, 10, 70
+    );
+
+
+    // display timeout
+    m_vc.push_back(std::make_shared<shs::ValueControl<uint16_t>>(
+        m_storage->cs_config.SLEEP_TIMEOUT / (1000 * 60), 1,
+        m_tft,
+        0, 0, 150, 40
+    ));
+
+    attachLayer(m_vc.back(), shs::Widget::Align::RIGHT | shs::Widget::Align::TOP, 10, 60 + 55);
+    attachLayer(std::make_shared<shs::Label>(
+        m_tft, F("Display timeout (min):"),
+        2, 1
+    ),
+        shs::Widget::LEFT | shs::Widget::TOP, 10, 70 + 55
+    );
+
+
+    // LED timeout
+    m_vc.push_back(std::make_shared<shs::ValueControl<uint16_t>>(
+        m_storage->cs_config.LED_TIMEOUT / (1000 * 60), 1,
+        m_tft,
+        0, 0, 150, 40
+    ));
+
+    attachLayer(m_vc.back(), shs::Widget::Align::RIGHT | shs::Widget::Align::TOP, 10, 60 + 55 * 2);
+    attachLayer(std::make_shared<shs::Label>(
+        m_tft, F("LED timeout (min):"),
+        2, 1
+    ),
+        shs::Widget::LEFT | shs::Widget::TOP, 10, 70 + 55 * 2
+    );
 
     Widget::start();
     Widget::tick();
 }
+
+void shs::SettingsWindow::tick()
+{
+    Widget::tick();
+
+
+    if (m_vc[0]->value != m_storage->cs_config.LED_BRIGHTNESS)
+    {
+        if (m_vc[0]->value > 255) m_vc[0]->value = 0;
+        m_storage->cs_config.LED_BRIGHTNESS = m_vc[0]->value;
+        m_config_changed = true;
+    }
+
+
+    if (m_vc[1]->value != m_storage->cs_config.SLEEP_TIMEOUT)
+    {
+        m_storage->cs_config.SLEEP_TIMEOUT = m_vc[1]->value * 1000 * 60;
+        m_config_changed = true;
+    }
+
+
+    if (m_vc[2]->value != m_storage->cs_config.LED_TIMEOUT)
+    {
+        m_storage->cs_config.LED_TIMEOUT = m_vc[2]->value * 1000 * 60;
+        m_config_changed = true;
+    }
+
+}
+
+
+
--- src/ClimateStation/SettingsWindow.h
+++ src/ClimateStation/SettingsWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 
 #include <TFT_eSPI.h>
 
@@ -9,6 +10,7 @@
 #include "GUIcore/RectWidget.h"
 #include "GUIcore/shs_ThemeColors.h"
 #include "GUIcore/IndicatorWidget.h"
+#include "GUIcore/ValueControl.h"
 
 #include "CO2_chart.h"
 
@@ -31,7 +33,9 @@ public:
     SettingsWindow(std::shared_ptr<TFT_eSPI> tft, std::shared_ptr<shs::ClimateStation> climate_station, std::shared_ptr<shs::ClimateStationStorage> storage);
 
     void start() override;
-    void tick()override;
+    void tick() override;
+
+    bool configChanged() { return m_config_changed; }
 
     std::vector<std::shared_ptr<shs::Widget>> buttons;
 
@@ -39,4 +43,11 @@ protected:
     static constexpr auto m_BUTTON_SIZE = 40;
     std::shared_ptr<shs::ClimateStationStorage> m_storage;
     std::shared_ptr<shs::ClimateStation> m_climate_station;
+    std::vector<std::shared_ptr<shs::ValueControl<uint16_t>>> m_vc;
+
+    // To store the touch coordinates
+    uint16_t m_touch_x{};
+    uint16_t m_touch_y{};
+
+    bool m_config_changed{};
 };
