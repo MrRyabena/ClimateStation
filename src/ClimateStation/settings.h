23e20529facf653028e71ac48ef6e97026b7904f
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-08-13 08:31:38 +0300

    Corrected current configuration.

--- src/ClimateStation/settings.h
+++ src/ClimateStation/settings.h
@@ -7,7 +7,7 @@
 static constexpr auto LEDS_NUM = 9 * 5;
 static constexpr auto LEDS_PIN = 16;
 
-static constexpr auto SD_CS = 3;
+static constexpr auto SD_CS = 1;
 static constexpr auto SD_MOSI = 23;
 static constexpr auto SD_MISO = 34;
 static constexpr auto SD_SCK = 18;
--- src/ClimateStation/shs_ClimateStationConfig.h
+++ src/ClimateStation/shs_ClimateStationConfig.h
@@ -23,7 +23,7 @@ struct shs::ClimateStationConfig
     shs::t::shs_pin_t MHZ19_RX_PIN = 35;
 
     // DHT
-    shs::t::shs_pin_t DHT_PIN = 1;
+    shs::t::shs_pin_t DHT_PIN = 3;
 
     // flags
     /*
@@ -31,7 +31,7 @@ struct shs::ClimateStationConfig
        MHZ19_b = 0b1, BME280_b = 0b10, DHT_b = 0b100,
        DS3231_b = 0b1000, NTP_b = 0b10000, UPDATING_TIMEOUT_b = 0b100000,
     */
-    uint8_t sensors_flags = 0b11111;
+    uint8_t sensors_flags = 0b11110;
 
     // -------------------------------INDICATION--------------------------------
 
