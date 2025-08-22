8b542bbbf2ce09d7a5bd0c4482e50dc1d6c8fe29
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2025-08-22 20:19:03 +0300

    Added default bot token.

--- src/ClimateStation/settings.h
+++ src/ClimateStation/settings.h
@@ -1,8 +1,13 @@
 #pragma once
 
 // #define DEBUG
+#include <shs_settings.h>
 
 #define CLIMATE_STATION_ESP32_VERSION "1.0.6"
+
+#define SHS_CS_DEFAULT_TBOT_TOKEN CS_TBOT_VVR_TOKEN
+// CS_TBOT_KULIC_TOKEN 
+// CS_TBOT_VVR_TOKEN
 
 static constexpr auto LEDS_NUM = 9 * 5;
 static constexpr auto LEDS_PIN = 16;
