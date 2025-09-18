4561003e440387b96c578424c77ea580b0c69f45
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-09-18 23:21:50 +0300

    Delete unused constants.

--- src/ClimateStation/settings.h
+++ src/ClimateStation/settings.h
@@ -1,19 +1,14 @@
 #pragma once
 
-// #define DEBUG
 #include <shs_settings.h>
 
 #define CLIMATE_STATION_ESP32_VERSION "1.0.6"
 
-#define SHS_CS_DEFAULT_TBOT_TOKEN CS_TBOT_VVR_TOKEN
+#define SHS_CS_DEFAULT_TBOT_TOKEN SHS_UpdateBot_TOKEN
 // CS_TBOT_KULIC_TOKEN 
 // CS_TBOT_VVR_TOKEN
 
 static constexpr auto LEDS_NUM = 9 * 5;
 static constexpr auto LEDS_PIN = 16;
 
-static constexpr auto SD_CS = 1;
-static constexpr auto SD_MOSI = 23;
-static constexpr auto SD_MISO = 34;
-static constexpr auto SD_SCK = 18;
 
